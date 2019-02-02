739f7c5bdbdc0a39918e1c3ab6a005c2dcd85b8b
Thirumalainambi14 <thirumalainambi1411@gmail.com>
2019-02-02 23:23:27 +0530

    thiruf

--- Test/app/src/main/java/com/example/test/Expenses_Activity.java
+++ Test/app/src/main/java/com/example/test/Expenses_Activity.java
@@ -1,5 +1,7 @@
 package com.example.test;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,12 +18,13 @@ public class Expenses_Activity extends AppCompatActivity {
     private Spinner listspinner;
     ArrayAdapter<CharSequence> adapter;
     static int prio;
-    static int Values;
+    static double Values;
     private EditText expenseName;
     private EditText expensevalues;
+    Button btnviewAll;
     private Button addDatabut;
-
-
+    DatabaseHelper myDb1;
+ double sum=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +34,9 @@ public class Expenses_Activity extends AppCompatActivity {
         expenseName=(EditText)findViewById(R.id.Sources);
         expensevalues=(EditText)findViewById(R.id.expenses_values);
         addDatabut=(Button)findViewById(R.id.saveButton);
+        myDb1 = new DatabaseHelper(this);
 
-
+        btnviewAll=(Button)findViewById(R.id.button_viewAll);
 
 
         adapter=ArrayAdapter.createFromResource(this,R.array.expense_list,
@@ -57,16 +61,58 @@ public class Expenses_Activity extends AppCompatActivity {
         addDatabut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Values = Integer.parseInt(expensevalues.getText().toString());
-
-
+                boolean isInserted = myDb1.insertData(expenseName.getText().toString(),Values=Double.parseDouble(expensevalues.getText().toString()));
+                if(isInserted == true)
+                    Toast.makeText(getBaseContext(),"Data is Inserted",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(getBaseContext(),"Data is Not Inserted",Toast.LENGTH_LONG).show();
             }
         });
 
+        viewAll();
 
 
     }
 
 
+    public void viewAll()
+    {
+        btnviewAll.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = myDb1.getAllData();
+                        if(res.getCount() == 0)
+                        {
+                            //show message error
+                            showMessage("Error404","No Data Found");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+                        while(res.moveToNext())
+                        {
+                            buffer.append("Id : " + res.getString(0) + "\n");
+                            buffer.append("Expenses : " + res.getString(1) + "\n");
+                            buffer.append("Values : " + res.getDouble(2) + "\n\n");
+                            sum = sum + res.getDouble(2);
+                        }
+                        //show all data
+                        showMessage("Data",buffer.toString());
+                        Toast.makeText(getBaseContext(),"Total Value is " + sum,Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+    }
+
+    public void showMessage(String title,String Message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
 
 }
