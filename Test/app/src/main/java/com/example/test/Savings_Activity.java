5963bd770b324bbc75f00e4e10359df23e537826
KarthikRaj <naidukarthi2193@gmail.com>
2019-02-02 22:27:10 +0530

    "Added calender"

--- Test/app/src/main/java/com/example/test/Savings_Activity.java
+++ Test/app/src/main/java/com/example/test/Savings_Activity.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 import android.animation.FloatArrayEvaluator;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -10,7 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import java.text.DecimalFormat;
-
+import android.view.View.OnFocusChangeListener;
 import java.text.NumberFormat;
 
 
@@ -22,6 +23,27 @@ public class Savings_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_savings);
 
+
+            EditText txtDate=(EditText)findViewById(R.id.txtdate);
+
+
+
+            txtDate.setOnFocusChangeListener(new OnFocusChangeListener(){
+                public void onFocusChange(View view, boolean hasfocus){
+                    if(hasfocus){
+                        com.example.test.DateDialog dialog=new com.example.test
+                                .DateDialog(view);
+                        FragmentTransaction ft =getFragmentManager().beginTransaction();
+                        dialog.show(ft, "DatePicker");
+
+
+
+
+
+                    }
+                }
+
+            });
 
 
 
